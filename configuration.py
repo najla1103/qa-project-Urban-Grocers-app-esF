4249282114ba303243dee32b00aa58fc4aef3712
najla1103 <najlasanchez@hotmail.com>
2024-07-18 21:39:27 -0600

    Actualizacion ReadMe

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-8a78f499-0f3b-42b6-bc69-1e0f77d4021c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-8ef12d8a-131c-40ff-ba87-66a865d3fcea.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
